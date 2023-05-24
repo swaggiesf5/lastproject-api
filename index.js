3405518ed10e5b0a1fec99080d8af1fdd81db269
swaggiesf5 <srirachat01@gmail.com>
2023-05-25 05:50:32 +0700

    lastone

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const jwt = require('jsonwebtoken');
 const secret = 'Fullstacken-Login';
-const cors = require('cors')
+const cors = require('cors');
 
 
 
@@ -16,6 +16,8 @@ const connection = mysql.createConnection(process.env.DATABASE_URL);
 console.log('Connected to PlanetScale!');
 
 app.use(cors())
+
+
 
 // Sign up a new user
 app.post('/SignUp', jsonParser, (req, res) => {
@@ -73,21 +75,36 @@ app.post('/SignIn', jsonParser, (req, res) => {
     'SELECT * FROM `user` WHERE email = ? AND password = ?',
     [email, password],
     function(err, user, fields) {
-      if(err){
-        res.json({status:'error', message: err});
+      if (err) {
+        res.json({ status: 'error', message: err });
         return;
       }
-      if(user.length == 0){
-        res.json({status:'error', message: 'Fail'});
+      if (user.length == 0) {
+        res.json({ status: 'error', message: 'Fail' });
         return;
       }
-      const token = jwt.sign({ id: user[0].id, email: user[0].email }, secret,{ expiresIn: '1h'});
-      res.json({status: 'ok', message: 'login sucess', token: token});
+      const token = jwt.sign({ id: user[0].id, email: user[0].email }, secret, { expiresIn: '1h' });
+      res.json({ status: 'ok', message: 'login success', token: token, id: user[0].id });
     }
   );
 });
 
-app.post('/authen', jsonParser, (req, res) => {
+const authenticateToken = (req, res, next) => {
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, secret, (err, decoded) => {
+    if (err) {
+      res.json({ status: 'Error', message: err.message });
+      return;
+    }
+    req.user = decoded;
+    next();
+  });
+};
+app.post('/authen', authenticateToken, (req, res) => {
+  res.json({ status: 'OK', decoded: req.user });
+});
+
+/*app.post('/authen', jsonParser, (req, res) => {
 
   try {
     const token = req.headers.authorization.split(' ')[1]
@@ -96,7 +113,10 @@ app.post('/authen', jsonParser, (req, res) => {
   } catch (err) {
     res.json({status: 'Error',message: err.message})
   }
-});
+});*/
+
+
+
 
 app.get('/user', jsonParser, (req, res) => {
   connection.query(
@@ -132,6 +152,8 @@ app.get('/user/:id', jsonParser, (req, res) => {
   );
 });
 
+
+
 // Get a vaccine by ID
 app.get('/vaccine/:id', jsonParser, (req, res) => {
   const vaccineId = req.params.id;
@@ -147,29 +169,14 @@ app.get('/vaccine/:id', jsonParser, (req, res) => {
     }
   );
 });
-app.delete('/user/:id', jsonParser, (req, res) => {
-  const vaccineId = req.params.id;
+
+
+app.delete('/user/delete/:id', jsonParser, (req, res) => {
+  const userId = req.params.id;
   connection.execute(
     'DELETE FROM `user` WHERE id = ?',
-    [vaccineId],
+    [userId],
     function(err, results, fields) {
-      if (err) {
-        res.json({ status: 'error', message: err });
-        return;
-      }
-      res.json({ status: 'OK', message: 'Vaccine deleted successfully' });
-    }
-  );
-});
-
-
-app.delete('/user/delete', jsonParser, (req, res) => {
-  const { id } = req.body;
-
-  connection.execute(
-    'DELETE FROM `user` WHERE id = ?',
-    [id],
-    (err, results, fields) => {
       if (err) {
         res.json({ status: 'error', message: err });
         return;
@@ -178,7 +185,6 @@ app.delete('/user/delete', jsonParser, (req, res) => {
     }
   );
 });
-
 
 // Update a user
 app.put('/user/:id', jsonParser, (req, res) => {
@@ -198,6 +204,62 @@ app.put('/user/:id', jsonParser, (req, res) => {
   );
 });
 
+app.put('/user/update/:id', jsonParser, (req, res) => {
+  const { id } = req.params;
+  const { idcard, fname, lname, email, pnumber, gender, date, password } = req.body;
+
+  connection.execute(
+    'UPDATE `user` SET idcard = ?, fname = ?, lname = ?, email = ?, pnumber = ?, gender = ?, date = ?, password = ? WHERE id = ?',
+    [idcard, fname, lname, email, pnumber, gender, date, password, id],
+    (err, results, fields) => {
+      if (err) {
+        res.json({ status: 'error', message: err });
+        return;
+      }
+      res.json({ status: 'OK', message: 'User updated successfully' });
+    }
+  );
+});
+
+app.post('/ChooseHospital', authenticateToken, jsonParser, (req, res) => {
+  const { hospitalId , vaccineId} = req.body;
+  const userId = req.user.id;
+
+  connection.execute(
+    'UPDATE `vaccine` SET hospital = ?, vactype = ?, WHERE id = ?',
+    [hospitalId, vaccineId ,userId],
+    (err, results, fields) => {
+      if (err) {
+        res.json({ status: 'error', message: err });
+        return;
+      }
+      res.json({ status: 'OK', message: 'Hospital selected successfully' });
+    }
+  );
+});
+
+
+app.get('/user/hospital/:id', jsonParser, (req, res) => {
+  const userId = req.params.id;
+
+  connection.execute(
+    'SELECT v.hospital, h.label FROM `vaccine` AS v JOIN `hospital` AS h ON v.hospital = h.id WHERE v.id = ?',
+    [userId],
+    function(err, results, fields) {
+      if (err) {
+        res.json({ status: 'error', message: err });
+        return;
+      }
+      res.send(results);
+    }
+  );
+});
+
+
+
+
+
+
 // Delete a vaccine
 app.delete('/vaccine/:id', jsonParser, (req, res) => {
   const vaccineId = req.params.id;
@@ -214,24 +276,8 @@ app.delete('/vaccine/:id', jsonParser, (req, res) => {
   );
 });
 
-app.delete('/vaccine/delete', jsonParser, (req, res) => {
-  const { id } = req.body;
-
-  connection.execute(
-    'DELETE FROM `vaccine` WHERE id = ?',
-    [id],
-    (err, results, fields) => {
-      if (err) {
-        res.json({ status: 'error', message: err });
-        return;
-      }
-      res.json({ status: 'OK', message: 'User deleted successfully' });
-    }
-  );
-});
-
 // Update a vaccine
-app.put('/vaccine/:id', jsonParser, (req, res) => {
+app.put('/vaccine/id/:id', jsonParser, (req, res) => {
   const vaccineId = req.params.id;
   const { id, hospital, vactype } = req.body;
 
@@ -266,9 +312,108 @@ app.put('/vaccine/:id', jsonParser, (req, res) => {
   );
 });
 
+app.put('/vaccine/both', jsonParser, (req, res) => {
+  const vaccineId = req.params.id;
+  const { hospital, vactype } = req.body;
+
+  connection.execute(
+    'UPDATE `vaccine` SET  hospital = ?, vactype = ? WHERE id = ?',
+    [hospital, vactype, vaccineId],
+    (err, results, fields) => {
+      if (err) {
+        res.json({ status: 'error', message: err });
+        return;
+      }
+      res.json({ status: 'OK', message: 'Vaccine updated successfully' });
+    }
+  );
+});
+
+app.put('/vaccine/hospital/:id', jsonParser, (req, res) => {
+  const vaccineId = req.params.id;
+  const { hospital } = req.body;
+
+  connection.execute(
+    'UPDATE `vaccine` SET hospital = ? WHERE id = ?',
+    [hospital, vaccineId],
+    (err, results, fields) => {
+      if (err) {
+        res.json({ status: 'error', message: err });
+        return;
+      }
+      res.json({ status: 'OK', message: 'Vaccine hospital updated successfully' });
+    }
+  );
+});
+
+app.put('/vaccine/id/:id', jsonParser, (req, res) => {
+  const vaccineId = localStorage.getItem('id'); // Retrieve the id from localStorage
+  const { hospital, vactype } = req.body;
+
+  connection.execute(
+    'UPDATE `vaccine` SET  hospital = ?, vactype = ? WHERE id = ?',
+    [hospital, vactype, vaccineId],
+    (err, results, fields) => {
+      if (err) {
+        res.json({ status: 'error', message: err });
+        return;
+      }
+      res.json({ status: 'OK', message: 'Vaccine updated successfully' });
+    }
+  );
+});
 
 
 
+app.put('/vaccine/vactype/:id', jsonParser, (req, res) => {
+  const vaccineId = req.params.id;
+  const { vactype } = req.body;
+
+  connection.execute(
+    'UPDATE `vaccine` SET vactype = ? WHERE id = ?',
+    [vactype, vaccineId],
+    (err, results, fields) => {
+      if (err) {
+        res.json({ status: 'error', message: err });
+        return;
+      }
+      res.json({ status: 'OK', message: 'Vaccine hospital updated successfully' });
+    }
+  );
+});
+
+
+app.delete('/user/delete', jsonParser, (req, res) => {
+  const { id } = req.body;
+
+  connection.execute(
+    'DELETE FROM `user` WHERE id = ?',
+    [id],
+    (err, results, fields) => {
+      if (err) {
+        res.json({ status: 'error', message: err });
+        return;
+      }
+      res.json({ status: 'OK', message: 'User deleted successfully' });
+    }
+  );
+});
+
+app.delete('/vaccine/delete', jsonParser, (req, res) => {
+  const { id } = req.body;
+
+  connection.execute(
+    'DELETE FROM `vaccine` WHERE id = ?',
+    [id],
+    (err, results, fields) => {
+      if (err) {
+        res.json({ status: 'error', message: err });
+        return;
+      }
+      res.json({ status: 'OK', message: 'User deleted successfully' });
+    }
+  );
+});
 
 
 app.listen(port, () => {
